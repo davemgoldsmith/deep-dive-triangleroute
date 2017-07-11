c365bddb1c71ca6a6853a11bfae602f28e8a57f1
Dave <davemgoldsmith@yahoo.com>
2017-07-11 16:27:29 -0600

    file io

---
+++ src/edu/cnm/deepdive/triangle/Solver.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package edu.cnm.deepdive.triangle;
+
+/**
+ * @author davem
+ *
+ */
+public class Solver {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	Data data = new Data("data/data.txt");
+
+	}
+
+}
