c89e7914ae85ac85e39bc9b6b4acfcde2da97912
Dave <davemgoldsmith@yahoo.com>
2017-07-12 15:46:27 -0600

    triangle

--- src/edu/cnm/deepdive/triangle/Solver.java
+++ src/edu/cnm/deepdive/triangle/Solver.java
@@ -3,6 +3,8 @@
  */
 package edu.cnm.deepdive.triangle;
 
+import java.util.Arrays;
+
 /**
  * @author davem
  *
@@ -13,8 +15,31 @@ public class Solver {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-	Data data = new Data("data/data.txt");
+		Data data = new Data("data/data.txt");
+		// System.out.println(Arrays.toString(data.nodes));
+		Route[] routes = null;
+		for (int i = data.nodes.length - 1; i >=  0; i--){
+			routes = processRow(data.nodes[i], routes);
+		}
+		System.out.printf("Minimum route cost = %, d%n",  routes[0].cumulativeCost);
+		System.out.print("steps: ");
+		for (Route trace = routes[0]; trace != null; trace = trace.optimal){
+			System.out.print(trace.direction);
+		}
+	}
 
+	private static Route[] processRow(int[] costs, Route[] routesBelow) {
+		Route[] result = new Route[costs.length];
+		if (routesBelow == null) {
+			for (int i = 0; i < costs.length; i++) {
+				result[i] = new Route(costs[i]);
+			}
+		} else {
+			for (int i = 0; i < costs.length; i++) {
+				result[i] = new Route(costs[i], routesBelow[i], routesBelow[i + 1]);
+			}
+		}
+		return result;
 	}
 
 }
